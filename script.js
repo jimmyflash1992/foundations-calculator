6859bef29f4fa0b485fb8031e952a089979733e4
jimmyflash1992 <jaymesrath@gmail.com>
2025-05-12 14:08:35 +1000

    add operate function to js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-function operate (operator, num1, num2) {
-    
+function operate(operator, num1, num2) {
+    let answer;
+
+    switch (operator) {
+
+        case '+':
+            answer = num1 + num2;
+            break;
+
+        case '-':
+            answer = num1 - num2;
+            break;
+        case '*':
+            answer = num1 * num2;
+            break;
+        case '/':
+            answer = num1 / num2;
+            break;
+    }
+
 }
+
+console.log(operate('*',6,5));
