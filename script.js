3c5e1a7bde570a92393c14c6f647615c2996013e
jimmyflash1992 <jaymesrath@gmail.com>
2025-05-13 14:58:40 +1000

    add start of logic for buttons

--- script.js
+++ script.js
@@ -22,4 +22,24 @@ function operate(operator, num1, num2) {
 
 }
 
-console.log((operate('/',6,5)));
+// console.log((operate('/',6,5)));
+
+
+//create function that displays numbers on screen when clicked
+
+
+const btn = document.querySelectorAll("button");
+const screen = document.querySelector("class");
+
+// store the button number in the display and also add to a variable for next function and return it
+btn.forEach(btn => {
+    btn.addEventListener("click", () => {
+    this.
+
+
+    }}
+        
+
+
+
+})
