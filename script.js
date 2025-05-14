95505f3465606fc859762296329537fc651fa943
jimmyflash1992 <jaymesrath@gmail.com>
2025-05-14 15:13:28 +1000

    update buttons to display in h1

--- script.js
+++ script.js
@@ -22,24 +22,25 @@ function operate(operator, num1, num2) {
 
 }
 
-// console.log((operate('/',6,5)));
+console.log((operate('/', 6, 5)));
 
 
 //create function that displays numbers on screen when clicked
 
 
 const btn = document.querySelectorAll("button");
-const screen = document.querySelector("class");
+const screen = document.querySelector('.result');
 
 // store the button number in the display and also add to a variable for next function and return it
-btn.forEach(btn => {
-    btn.addEventListener("click", () => {
-    this.
-
-
-    }}
+btn.forEach(button => {
+    button.addEventListener("click", () => {
+        
+        
+        screen.innerHTML = button.innerHTML;
         
 
+        console.log(button.innerHTML);
 
 
+    })
 })
